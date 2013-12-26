d8e0c4139db9a24e218702e8885eb61281f106df
Gance <gance@feltmeng.com>
2013-12-26 11:26:15 +0800

    Fixed a bug of using wrong collection in Miix movie generation

--- miix_content_mgr.js
+++ miix_content_mgr.js
@@ -38,7 +38,7 @@ var miixContentMgr = {};
  */
 miixContentMgr.generateMiixMoive = function(movieProjectID, ownerStdID, ownerFbID, movieTitle, cbOfGenerateMiixMoive) {
     
-    var ugcModel = db.getDocModel("fakeUgc");
+    var ugcModel = db.getDocModel("ugc");
     var errDuringProcessing = null;
     var urls = null;
     var fileExtension = null;
